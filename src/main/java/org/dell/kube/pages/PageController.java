f285f70dcc9f20e15a39e8ef31f7ea5c9ff9d06c
boonkc <desserts.noob@gmail.com>
2020-10-30 03:02:04 +0000

    added logging

--- src/main/java/org/dell/kube/pages/PageController.java
+++ src/main/java/org/dell/kube/pages/PageController.java
@@ -3,29 +3,50 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import ch.qos.logback.classic.Logger;
+import org.slf4j.LoggerFactory;
+
 @RestController
 @RequestMapping("/pages")
 public class PageController {
     private IPageRepository pageRepository;
+    Logger logger =(Logger)LoggerFactory.getLogger(this.getClass());
     public PageController(IPageRepository pageRepository)
     {
         this.pageRepository = pageRepository;
     }
     @PostMapping
     public ResponseEntity<Page> create(@RequestBody Page page) {
+        logger.info("CREATE-INFO:Trying to create page for = " + page.businessName);
+        logger.debug("CREATE-DEBUG:Trying to create page for = " + page.businessName);
         Page newPage= pageRepository.create(page);
+
+        logger.info("CREATE-INFO:Created  page for = " + page.businessName + "with id = " + newPage.id);
+        logger.debug("CREATE-DEBUG:Created  page for = " + page.businessName + "with id = " + newPage.id);
         return new ResponseEntity<Page>(newPage, HttpStatus.CREATED);
+
+
     }
+
     @GetMapping("{id}")
     public ResponseEntity<Page> read(@PathVariable long id) {
+
+        logger.info("READ-INFO:Fetching page with id = " + id);
+        logger.debug("READ-DEBUG:Fetching page with id = " + id);
+
         Page page = pageRepository.read(id);
         if(page!=null)
             return new ResponseEntity<Page>(page, HttpStatus.OK);
-        else
+        else {
+            logger.error("READ-ERROR:Could not find page with id = " + id);
             return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
     }
     @GetMapping
     public ResponseEntity<List<Page>> list() {
+        logger.info("LIST-INFO:Fetching all pages");
+        logger.debug("LIST-INFO:Fetching all pages");
+
         List<Page> pages= pageRepository.list();
         return new ResponseEntity<List<Page>>(pages, HttpStatus.OK);
     }
